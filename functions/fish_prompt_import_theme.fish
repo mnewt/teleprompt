7d7c276b02f1374d24e4db5cdf76a3ce34c1bf6a
Matthew Sojourner Newton <matt@knosis.org>
2017-10-18 13:08:12 -0700

    removed string replace --filter so that we could remain compatible with fish 2.2-2.5

--- functions/fish_prompt_import_theme.fish
+++ functions/fish_prompt_import_theme.fish
@@ -4,8 +4,8 @@ function fish_prompt_import_theme
   set key_value_regex '^([a-zA-Z_][a-zA-Z0-9_]*)(?:=(.*))?'
 
   for line in $file_contents
-    if set key (string replace -fr "$key_value_regex" '$1' -- $line)
-    and set value (string replace -fr "$key_value_regex" '$2' -- $line)
+    if set key (string replace -r "$key_value_regex" '$1' -- $line)
+    and set value (string replace -r "$key_value_regex" '$2' -- $line)
       set -g $key $value
     end
   end
